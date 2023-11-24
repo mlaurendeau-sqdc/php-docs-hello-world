4c892047f208661dc7b8f75c06c3ff0dd1144611
mlaurendeau-sqdc <151961943+mlaurendeau-sqdc@users.noreply.github.com>
2023-11-24 14:30:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 //echo "Hello World!";
 
 $homepage = file_get_contents('http://swint1.sqdc.ca:8080/SQDCInfosProduits?tv=2&succ=77105&page=0&timer=10');
-ereg("'(.*)', {1,}'(.*)'", $homepage, $regs);
+//ereg("'(.*)', {1,}'(.*)'", $homepage, $regs);
