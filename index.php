b467cdbbed68c641719ac76c0a9205573a1a488c
mlaurendeau-sqdc <151961943+mlaurendeau-sqdc@users.noreply.github.com>
2023-11-24 14:25:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 //echo "Hello World!";
 
-$homepage = file_get_contents('https://www.w3schools.com');
+$homepage = file_get_contents('http://swint1.sqdc.ca:8080/SQDCInfosProduits?tv=2&succ=77105&page=0&timer=10');
 ereg("'(.*)', {1,}'(.*)'", $homepage, $regs);
